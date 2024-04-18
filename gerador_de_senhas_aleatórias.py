391709cb9f6ac0570a277f8100a1a2f1d2577ff0
YanPeterson <71107144+YanPeterson@users.noreply.github.com>
2024-04-18 02:45:09 -0300

    Update gerador_de_senhas_aleatórias.py
    Colocando caracters na senha

--- gerador_de_senhas_aleatórias.py
+++ gerador_de_senhas_aleatórias.py
@@ -1,14 +1,15 @@
 import random as rd
 qtd_senha = int(input("Insira quantas senhas deseja gerar: "))
 x = 1
-y = 1
-lista_senha = [ ]
+cad_caract = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+lista_senha = []
 lista2 = []
 while x <= qtd_senha:
+    y = 1
     caracters = int(input("Quantos caracters dejesa nessa senha: "))
     while y <= caracters:
-        z = rd.randrange(0, 10)
-        lista2.append(str(z))
+        z = rd.randrange(0, 46)
+        lista2.append(str(cad_caract[z]))
         y = y + 1
     senha = ''.join(lista2)
     lista_senha.append(senha)
@@ -16,4 +17,3 @@ while x <= qtd_senha:
     y = 1
     lista2 = []
 print(lista_senha)
-    
