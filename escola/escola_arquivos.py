88934b8c35f67987143083ad1f687006372f09c8
YanPeterson <71107144+YanPeterson@users.noreply.github.com>
2024-06-28 19:47:26 -0300

    OOP

---
+++ escola/escola_arquivos.py
@@ -0,0 +1,14 @@
+class Aluno:
+    def __init__(self, nome, matricula, cpf):
+        self.nome = nome
+        self.matricula = matricula
+        self.cpf = cpf
+
+class Professor:
+    def __init__(self, nome, matricula, id, cpf, salario):
+        self.nome = nome
+        self.matricula = matricula
+        self.id = id
+        self.cpf = cpf
+        self.salário = salario
+
